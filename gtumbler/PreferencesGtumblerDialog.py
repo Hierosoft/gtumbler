43b47f2bb2fd604fec47c779094395fc8ee3e1bc
Gabriele N. Tornetta <phoenix1987@gmail.com>
2012-08-10 16:35:46 +0200

    First new version release

---
+++ gtumbler/PreferencesGtumblerDialog.py
@@ -0,0 +1,46 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+### BEGIN LICENSE
+# Copyright (C) 2012 Gabriele N. Tornetta <phoenix1987@gmail.com>
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License version 3, as published 
+# by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranties of 
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+# PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along 
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+### END LICENSE
+
+# This is your preferences dialog.
+#
+# Define your preferences in
+# data/glib-2.0/schemas/net.launchpad.gtumbler.gschema.xml
+# See http://developer.gnome.org/gio/stable/GSettings.html for more info.
+
+from gi.repository import Gio # pylint: disable=E0611
+
+import gettext
+from gettext import gettext as _
+gettext.textdomain('gtumbler')
+
+import logging
+logger = logging.getLogger('gtumbler')
+
+from gtumbler_lib.PreferencesDialog import PreferencesDialog
+
+class PreferencesGtumblerDialog(PreferencesDialog):
+    __gtype_name__ = "PreferencesGtumblerDialog"
+
+    def finish_initializing(self, builder): # pylint: disable=E1002
+        """Set up the preferences dialog"""
+        super(PreferencesGtumblerDialog, self).finish_initializing(builder)
+
+        # Bind each preference widget to gsettings
+        settings = Gio.Settings("net.launchpad.gtumbler")
+        widget = self.builder.get_object('chk_display_path')
+        settings.bind("example", widget, "active", Gio.SettingsBindFlags.DEFAULT)
+
+        # Code for other initialization actions should be added here.
---
+++ setup.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+### BEGIN LICENSE
+# Copyright (C) 2012 Gabriele N. Tornetta <phoenix1987@gmail.com>
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License version 3, as published 
+# by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranties of 
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+# PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along 
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+### END LICENSE
+
+###################### DO NOT TOUCH THIS (HEAD TO THE SECOND PART) ######################
+
+import os
+import sys
+
+try:
+    import DistUtilsExtra.auto
+except ImportError:
+    print >> sys.stderr, 'To build gtumbler you need https://launchpad.net/python-distutils-extra'
+    sys.exit(1)
+assert DistUtilsExtra.auto.__version__ >= '2.18', 'needs DistUtilsExtra.auto >= 2.18'
+
+def update_config(values = {}):
+
+    oldvalues = {}
+    try:
+        fin = file('gtumbler_lib/gtumblerconfig.py', 'r')
+        fout = file(fin.name + '.new', 'w')
+
+        for line in fin:
+            fields = line.split(' = ') # Separate variable from value
+            if fields[0] in values:
+                oldvalues[fields[0]] = fields[1].strip()
+                line = "%s = %s\n" % (fields[0], values[fields[0]])
+            fout.write(line)
+
+        fout.flush()
+        fout.close()
+        fin.close()
+        os.rename(fout.name, fin.name)
+    except (OSError, IOError), e:
+        print ("ERROR: Can't find gtumbler_lib/gtumblerconfig.py")
+        sys.exit(1)
+    return oldvalues
+
+
+def update_desktop_file(datadir):
+
+    try:
+        fin = file('gtumbler.desktop.in', 'r')
+        fout = file(fin.name + '.new', 'w')
+
+        for line in fin:            
+            if 'Icon=' in line:
+                line = "Icon=%s\n" % (datadir + 'media/gtumbler.svg')
+            fout.write(line)
+        fout.flush()
+        fout.close()
+        fin.close()
+        os.rename(fout.name, fin.name)
+    except (OSError, IOError), e:
+        print ("ERROR: Can't find gtumbler.desktop.in")
+        sys.exit(1)
+
+
+class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
+    def run(self):
+        values = {'__gtumbler_data_directory__': "'%s'" % (self.prefix + '/share/gtumbler/'),
+                  '__version__': "'%s'" % self.distribution.get_version()}
+        previous_values = update_config(values)
+        update_desktop_file(self.prefix + '/share/gtumbler/')
+        DistUtilsExtra.auto.install_auto.run(self)
+        update_config(previous_values)
+
+
+        
+##################################################################################
+###################### YOU SHOULD MODIFY ONLY WHAT IS BELOW ######################
+##################################################################################
+
+DistUtilsExtra.auto.setup(
+    name='gtumbler',
+    version='3.0',
+    license='GPL-3',
+    author='Gabriele N. Tornetta',
+    author_email='phoenix1987@gmail.com',
+    #description='UI for managing …',
+    #long_description='Here a longer description',
+    url='https://launchpad.net/gtumbler',
+    cmdclass={'install': InstallAndUpdateDataDirectory}
+    )
+
